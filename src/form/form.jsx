f9c4a349cc4e21a26b222eabb79506f1f3cd10df
Akib Bagwan <akibbagwan69@gmail.com>
2024-03-03 19:55:10 +0530

    for deployment

---
+++ src/form/form.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+export default function Form() {
+  const [fullname, setFullname] = useState("");
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        setFullname(e.target.firstName.value + " " + e.target.lastName.value);
+      }}
+    >
+      <h1>Full Name Display</h1>
+      <label htmlFor="">First Name:</label>
+      <input required type="text" name="firstName" />
+      <br />
+      <label htmlFor="">Last Name:</label>
+      <input required type="text" name="lastName" />
+      <br />
+      <button>submit</button>
+      <p>Full Name: {fullname}</p>
+    </form>
+  );
+}
