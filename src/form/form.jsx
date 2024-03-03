8452f2462ff1147e2980e27caf0a8762df507965
Akib Bagwan <akibbagwan69@gmail.com>
2024-03-03 21:51:37 +0530

    state update

--- src/form/form.jsx
+++ src/form/form.jsx
@@ -1,22 +1,35 @@
 import { useState } from "react";
 export default function Form() {
-  const [fullname, setFullname] = useState("");
+  const [first, setFirstname] = useState("");
+  const [last, setLastname] = useState("");
+  const [show,setShow] = useState(false)
+
+   const handleChange = (e) => {
+     if (e.target.name === "firstName"){
+        setFirstname(e.target.value)
+     }
+     else{
+        setLastname(e.target.value)
+     }
+   }
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        setFullname(e.target.firstName.value + " " + e.target.lastName.value);
+        // if (first!=="" && last!=="")
+         setShow(true)
+        // setFullname(e.target.firstName.value + " " + e.target.lastName.value);
       }}
     >
       <h1>Full Name Display</h1>
       <label htmlFor="firstName">First Name:</label>
-      <input required type="text" name="firstName" />
+      <input onChange={handleChange} value={first} required type="text" name="firstName" />
       <br />
       <label htmlFor="lastName">Last Name:</label>
-      <input required type="text" name="lastName" />
+      <input onChange={handleChange} value={last} required type="text" name="lastName" />
       <br />
       <button type="submit">submit</button>
-      {fullname.length ? <p>Full Name: {fullname}</p> : ""}
+      {show && <p>Full Name: {first} {last}</p>}
     </form>
   );
 }
